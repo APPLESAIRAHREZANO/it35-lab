1f0c57cdb4a4da07d93022f663352eaae12f2de5
Apple <APPLESAIRAHREZANO@gmail.com>
2025-04-09 13:04:33 +0800

    Updates

--- assets/ios.transition-legacy-Dlt5iYRD.js
+++ assets/ios.transition-legacy-Dlt5iYRD.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dig6zkHg.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-C4yVrHTy.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
