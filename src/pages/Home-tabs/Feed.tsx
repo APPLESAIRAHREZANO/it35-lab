ede6f079bef2b5727205953c97fe83c0bf1e02c2
Apple <APPLESAIRAHREZANO@gmail.com>
2025-04-21 14:42:58 +0800

    added feed

--- src/pages/Home-tabs/Feed.tsx
+++ src/pages/Home-tabs/Feed.tsx
@@ -1,10 +1,5 @@
 import { 
   IonButtons,
-    IonCard,
-    IonCardContent,
-    IonCardHeader,
-    IonCardSubtitle,
-    IonCardTitle,
     IonContent, 
     IonHeader, 
     IonMenuButton, 
@@ -12,6 +7,8 @@ import {
     IonTitle, 
     IonToolbar 
 } from '@ionic/react';
+import FeedContainer from '../../components/FeedContainer';
+
 const Feed: React.FC = () => {
   return (
     <IonPage>
@@ -24,17 +21,10 @@ const Feed: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-      <IonCard>
-    <img alt="Silhouette of mountains" src="https://pbs.twimg.com/media/EtKtSEiXIAIvC1c?format=jpg&name=large" />
-    <IonCardHeader>
-      <IonCardTitle>Stitch</IonCardTitle>
-      <IonCardSubtitle> Lil' Trouble, Big Heart! </IonCardSubtitle>
-    </IonCardHeader>
-
-    <IonCardContent>Stitch, also known as Experiment 626, is a lovable yet mischievous alien character from Disney's Lilo & Stitch. Originally created by the mad scientist Dr. Jumbaa Jookiba to be a force of destruction, she crash-lands on Earth and ends up in Hawaii, where she meets a little girl named Lilo. Despite her chaotic nature, Stitch slowly learns the true meaning of love and family through her bond with Lilo. With her super strength, high intelligence, and quirky personality, she often finds herself in humorous and heartwarming situations. Over time, Stitch transforms from a wild experiment into a loyal and protective friend. Her journey beautifully reflects the message of “ohana,” a Hawaiian word meaning family — and family means no one gets left behind or forgotten.</IonCardContent>
-  </IonCard>
+        <FeedContainer />
       </IonContent>
     </IonPage>
   );
 };
+
 export default Feed;
