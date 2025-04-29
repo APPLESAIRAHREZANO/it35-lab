3b1516b92ace4c284244784e036fb2e83e6ac676
Apple <APPLESAIRAHREZANO@gmail.com>
2025-04-29 18:17:44 +0800

    Updates

--- assets/ios.transition-legacy-C4SNfwHh.js
+++ assets/ios.transition-legacy-C4SNfwHh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-bmGkjV6c.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DipSxDqF.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
