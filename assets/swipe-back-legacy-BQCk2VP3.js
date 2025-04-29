a048ef15a27bbcf7129a71e3ba7f9907f77e094c
Apple <APPLESAIRAHREZANO@gmail.com>
2025-04-29 12:16:30 +0800

    Updates

--- assets/swipe-back-legacy-BQCk2VP3.js
+++ assets/swipe-back-legacy-BQCk2VP3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CvYsDwh4.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-bmGkjV6c.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
